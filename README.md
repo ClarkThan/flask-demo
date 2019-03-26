b9087786f3325a4bd2fbcb35576fe3096fcc4de4
Clark Than <lenplex@gmail.com>
2019-03-26 18:28:23 +0800

    引入数据库

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 > 准备
 
-+ 安装python3  git  vscode[pycharm]等
-+ 安装pipenv: `pip install pipenv`
-+ 下载代码到本地: `git clone https://github.com/ClarkThan/flask-demo.git`
++ 更新代码: `git pull origin` 或者重新clone
 + 准备运行环境: `cd flask-demo && pipenv install`
 + 激活运行环境: `pipenv shell`
++ 准备数据库(也可单独创建): `python` 进入
+    + from app import app, db
+    + ctx = app.app_context(); ctx.push()
+    + db.create_all()
+    + ctx.pop()
++ 上一步成功的话, 当前目录会增加**test.db**文件
 + 运行程序: `python app.py`
 
 > 用例(分别浏览器中访问如下链接)
 
-+ http://127.0.0.1:5000/
-+ http://127.0.0.1:5000/no
-+ http://127.0.0.1:5000/hello/Tom
-+ http://127.0.0.1:5000/hello/Jerry
++ http://127.0.0.1:5000/tb
++ 另外插入n条数据: `sqlite3 test.db`
+    + insert into user values (3, 'Adam', 'adam@qq.com'), (4, 'John', 'john@qq.com');
++ http://127.0.0.1:5000/tb
--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/no')
 def index_no():
     return '这里没有使用模板文件, 直接返回<h2>字符串</h2>给前端浏览器'
+
+
+@app.route('/tb')
+def table():
+    result = '<table border="1">'
+
+    ds = db.session.query(User.name, User.email).limit(5).all()
+    for u in ds:
+        row = '<tr><td>{}</td><td>{}</td></tr>'.format(u.name, u.email)
+        result += row
+
+    result += '</table>'
+    return result
 
 
 @app.route('/')
@@ -18,7 +48,6 @@ def index():
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('hello.html', your_name=name)
-
 
 
 if __name__ == '__main__':
